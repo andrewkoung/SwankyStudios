eaf39ff9aaf9b5db03330b82685f3a256e5b9695
Andrew <andrewpkoung@gmail.com>
2020-11-13 21:16:49 -0500

    components_folder

---
+++ src/components/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Nav from '../../Generic/Navbar';
+import './style.css';
+
+function Home() {
+  return (
+    <div className="App"> 
+        <Nav />
+    </div>
+  );
+}
+
+export default Home;
